4ca3ca4ff59213e09c238ae7406cf5a370b2bb7f
Irina Egorova <irinabalerina@Irinas-MacBook-Air.local>
2020-09-20 23:45:08 +0300

    First commit. Testing Heroku

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+# This Python file uses the following encoding: utf-8
 import os
 import requests
 import telegram
@@ -6,30 +7,44 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению в работе нашлись ошибки.'
+    homework_name = homework.get('homework_name')
+    if homework.get('status') != 'approved':
+        verdict = u'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = u'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    params = {'from_date': current_timestamp}
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    try:
+        homework_statuses = requests.get(
+            'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+            headers=headers,
+            params=params,
+            timeout=10
+        )
+    except requests.exceptions.Timeout as err:
+        print(f'Timeout Error: {err}')
+    except requests.exceptions.HTTPError as err:
+        print(f'HTTP Error: {err}')
+    except requests.exceptions.ConnectionError as err:
+        print(f'Connection Error: {err}')
+    except requests.exceptions.RequestException as err:
+        print(f'Some error has happened: {err}')
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
@@ -40,8 +55,10 @@ def main():
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
+            else:
+                send_message('Вы не загрузили работу на проверку.')
             current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+            time.sleep(1200)  # опрашивать раз в двадцать минут
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
